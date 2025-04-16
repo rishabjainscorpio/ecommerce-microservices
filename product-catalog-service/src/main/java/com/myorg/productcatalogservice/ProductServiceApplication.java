db14cee53851f7007f7c32dfb5af847fbfe3ae7d
Rishab Jain <rishabjain555@gmail.com>
2025-04-16 14:01:38 +0530

    api gateway

--- product-catalog-service/src/main/java/com/myorg/productcatalogservice/ProductServiceApplication.java
+++ product-catalog-service/src/main/java/com/myorg/productcatalogservice/ProductServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ShoppingKartApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingKartApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
